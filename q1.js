4805a7380fc854f61d62125fb6fc2bd3b794fde0
Chris Sach <christophersach90@gmail.com>
2022-11-16 12:01:37 +0000

    testing to make sure it's pushing

---
+++ q1.js
@@ -0,0 +1,13 @@
+// Q1
+
+// It'll take two numbers and add them together. 
+// The value of result will the sum of the numbers, in this case 10
+
+function myFunction(num1, num2) {
+    return num1+num2
+}
+
+const result = myFunction(5,5)
+console.log(result)
+
+//Did exactly what we expected!
---
+++ q2.js
@@ -0,0 +1,13 @@
+//Q2
+
+//This function will add two numbers but will return undefined
+//as return operator is not there
+
+function myFunction(num1, num2) {
+    num1+num2
+}
+
+const result = myFunction(5,5)
+console.log(result)
+
+//Did exactly as we expected!
