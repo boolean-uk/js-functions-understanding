4d713f01513f27900a61f09491c595954706421d
JakeBurdass <jburdass@outlook.com>
2022-11-16 14:16:27 +0000

    Finished to Q10

---
+++ q10.js
@@ -0,0 +1,15 @@
+//Q10
+
+// 100 will be logged
+
+function myFunction(num1, num2, num3) {
+    console.log(num3)
+}
+
+const num1 = 10
+const num2 = 1
+const num3 = 20
+
+myFunction(num3, num1, 100)
+
+// Ran as planned
